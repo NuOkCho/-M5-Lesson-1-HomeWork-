3c610c74ba68b41679edad64e26ce3773b6f8231
Ahmed Muvaza <makbookicloud312@mail.ru>
2023-07-10 20:28:37 +0600

    Initial Commit

---
+++ (M5)Lesson-1(HomeWork)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  (M5)Lesson-1(HomeWork)
+//
+//  Created by Ahmed Muvaza on 10/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
